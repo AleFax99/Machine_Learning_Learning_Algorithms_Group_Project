6291a67a7e1d02bf64a55ebb15a2bf3624593dfd
Stan121 <staskiryllo@gmail.com>
2023-12-08 16:49:16 +0100

    lala

--- helloStas.R
+++ helloStas.R
@@ -1,1 +1,14 @@
 print("Hello i am stas")
+library(tidymodels)
+library(dplyr)
+
+load("offers_censored.RData")
+
+# Checking the proportions of Enrolled vs. not enrolled: Should be done on the 
+# training set only!!
+  # Do we need to downsample?
+
+offers %>%
+  group_by(Status) %>%
+  summarize(prop = n() / nrow(offers))
+
